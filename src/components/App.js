f1267ea5ac50ea28f0f69a46be353bd636d0ee37
fis007 <schaffhauserfilip@gmail.com>
2020-10-26 16:10:55 +0800

    new commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,31 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 
 class App extends Component {
   render() {
+    console.log("Props", this.props);
     return <div>Hello world</div>;
   }
 }
 
-export default App;
+function mapStateToProps(calendar) {
+  const dayOrder = [
+    "sunday",
+    "monday",
+    "tuesday",
+    "wednesday",
+    "thursday",
+    "friday",
+  ];
+  return {
+    calendar: dayOrder.map((day) => ({
+      day,
+      meals: Object.keys(calendar[day]).reduce((meals, meal) => {
+        meals[meal] = calendar[day][meal] ? calendar[day][meal] : null;
+        return meals;
+      }, {}),
+    })),
+  };
+}
+
+export default connect()(App);
