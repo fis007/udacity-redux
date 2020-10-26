be3cab67c600dc19374a605d6cc67481c0dde28e
fis007 <schaffhauserfilip@gmail.com>
2020-10-26 20:23:14 +0800

    finished

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,10 +1,12 @@
-const API_ID = process.env.REACT_APP_API_ID
-const APP_KEY = process.env.REACT_APP_APP_KEY
+const API_ID = process.env.REACT_APP_API_ID;
+const APP_KEY = process.env.REACT_APP_APP_KEY;
 
-export function fetchRecipes (food = '') {
-  food = food.trim()
+export function fetchRecipes(food = "") {
+  food = food.trim();
 
-  return fetch(`https://api.edamam.com/search?q=${food}&app_id=${API_ID}&app_key=${APP_KEY}`)
+  return fetch(
+    `https://api.edamam.com/search?q=${food}&app_id=${"7758f4fd"}&app_key=${"83edb18be7c69ac0a1363c2af49456ed"}`
+  )
     .then((res) => res.json())
-    .then(({ hits }) => hits.map(({ recipe }) => recipe))
+    .then(({ hits }) => hits.map(({ recipe }) => recipe));
 }
